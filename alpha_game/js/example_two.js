e3db0c1ba2ad394f1ebc263a05a09eeed6e5ed0b
MRomero <romerolaguado@gmail.com>
2022-10-18 11:46:02 -0500

    Aprendiendo cómo sacer los milisegundos, segundos para sacar cuántos FPS se genera.

--- alpha_game/js/example_two.js
+++ alpha_game/js/example_two.js
@@ -1,40 +1,15 @@
 window.onload = function() {
-    // Animaciones en el canvas:
-    let canvas = document.getElementById('canvas');
-    let context = canvas.getContext("2d");
-    let x = 0;
-    let y = 100;
+    // let test = Date.now();
+    // console.log(test);
+    let test;
 
     function draw() {
-        // Limpiar el canvas cuando se termina la animacion
-        context.clearRect(0, 0, 600, 400);
-        
-        context.beginPath();
-        context.rect(x, y, 100, 100);
-        context.fillStyle="red";
-        context.fill();
-
-        x += 5;
-        if (x >= 500) {
-            // start drawing another cube to the beginning when its right x position reaches the end of canvas
-            context.beginPath();
-            context.rect(x-600, y, 100, 100)
-            context.fillStyle="blue";
-            context.fill();
-            // when it reaches fully
-            // you reset it
-            if (x >= 600){
-                x = 0;
-            }
-        }
-
-        window.requestAnimationFrame(draw);
-      }
-      /*
-      Se puede reemplazar setInterval() con window.requestAnimationFrame() para evitar que, el navegador
-      del usuario, se vaya a bloquear en el redibujado, tomando en cuenta los recursos del sistema
-      para realizar el "game loop".
-       */
-    //   setInterval(draw, 30);
+        let timePassed = (Date.now() - test) / 1000;
+        let fps = Math.round(1 / timePassed);
+        test = Date.now();
+        console.log("Segundos: " + test);
+        console.log("FPS: " + fps);
+    }
+    draw();
     draw();
 }
