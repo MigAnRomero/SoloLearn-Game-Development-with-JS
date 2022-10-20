7e3bef61441ae9f136ae2c793f884851c75dfb34
MRomero <romerolaguado@gmail.com>
2022-10-20 10:28:24 -0500

    Creando nuevos archivos para poder usar los botones en el objeto creado en el canvas.

---
+++ alpha_game/js/user_input.js
@@ -0,0 +1,57 @@
+// Code para el segundos canvas
+window.onload = function() {
+    // Animaciones en el canvas:
+    let canvas = document.getElementById('canvas_dos');
+    let context = canvas.getContext("2d");
+    let x = 0;
+    let y = 100;
+    let test = Date.now();
+    // Mover un objeto a 100px por segundo:
+    let speed = 100;
+    // Direccion del momvimiento para el objeto:
+    let direct = 1;
+    // Codigo para los botones:
+    let btn_up = document.getElementById('up');
+    let btn_left = document.getElementById('left');
+    let btn_down = document.getElementById('down');
+    let btn_right = document.getElementById('right');
+
+    function draw() {
+        let timePassed = (Date.now() - test) / 1000;
+        test = Date.now(); 
+        let fps = Math.round(1 / timePassed);
+    
+        // Limpiar el canvas cuando se termina la animacion
+        context.clearRect(0, 0, 600, 400);
+        
+        context.beginPath();
+        context.font = '42px Arial';
+        context.fillStyle = 'black';
+        context.fillText("FPS: " + fps, 0, 30);
+        context.fillText("Time Passed: " + timePassed, 195, 30);
+        context.fill();
+
+        context.beginPath();
+        context.rect(x, y, 100, 100);
+        context.fillStyle="red";
+        context.fill();
+
+        // x += 10;
+        // x += (speed * timePassed);
+        // Direccion del objeto:
+        x += direct * (speed * timePassed);
+        
+        if (x >= 600 - 100 || x <= 0) {
+            // speed = 0;
+            direct *= -1;
+        }
+
+        // if (x >= 600) {
+        //     x = -100;
+        // }
+
+        window.requestAnimationFrame(draw);
+      }
+
+    draw();
+}
