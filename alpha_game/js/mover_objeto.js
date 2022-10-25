4d655eab055ef6df71b255e2f2f4594a8ed5e21a
Miguel Romero <romerolaguado@gmail.com>
2022-10-24 21:26:11 -0500

    Buscar el problema que impide mover el objeto en el canvas.

--- alpha_game/js/mover_objeto.js
+++ alpha_game/js/mover_objeto.js
@@ -2,6 +2,7 @@ window.onload = function() {
     const canvas = document.getElementById("lienzo");
     const context = canvas.getContext("2d");
     let objeto = [];
+    let objetoActual = null;
     // Agregar objetos de prueba
     objeto.push({
         x: 0, y: 0, width: 100, height: 200, color: '#00f'
@@ -13,6 +14,8 @@ window.onload = function() {
 
     // Mostrar los objetos en el canvas
     function draw() {
+        context.fillStyle = '#f0f0f0';
+        context.fillRect(0, 0, 600, 400);
         for(let i = 0; i < objeto.length; i++) {
             context.fillStyle = objeto[i].color;
             context.fillRect(objeto[i].x, objeto[i].y, objeto[i].width, objeto[i].height);
@@ -20,4 +23,23 @@ window.onload = function() {
     }
 
     draw();
+
+    // Definir los eventos del mouse
+    canvas.onmousedown = function(event) {
+        for(let i = 0; i < objeto.length; i++) {
+            if(objeto[i].x < event.clientX 
+                && (objeto[i].width + objeto[i].x > event.clientX)) {
+                    objetoActual = objeto[i];
+                    break;
+                }
+        }
+    };
+
+    canvas.onmousemove = function(event) {
+        if(objetoActual != null) {
+            objetoActual.x = event.clientX;
+            objetoActual.y = event.clientY;
+        }
+        draw();
+    };
 };
