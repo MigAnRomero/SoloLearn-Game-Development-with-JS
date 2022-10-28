0527950c15e844d3dac0ddd569317a275a9ed85f
MRomero <romerolaguado@gmail.com>
2022-10-28 10:47:29 -0500

    Colocando un import del CDN copiado para probar la librería P5JS.

--- alpha_game/js/snake_game.js
+++ alpha_game/js/snake_game.js
@@ -1,1 +1,17 @@
-window.onload = function() {};
+import {} from "https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.5.0/p5.min.js";
+
+window.onload = function() {
+    const COLUMNAS = 40;
+    const FILAS = 30;
+    const LADO = 20;
+    const ANCHO_CANVAS = COLUMNAS * LADO;
+    const ALTO_CANVAS = FILAS * LADO;
+
+    function setup() {
+        createCanvas(ANCHO_CANVAS, ALTO_CANVAS);
+    }
+
+    function draw() {
+        background("black");
+    }
+};
