23e11b2b345f2420a82a669cd927e7150299606f
MRomero <romerolaguado@gmail.com>
2022-10-28 11:15:21 -0500

    Corrigiendo el error para mostrar el canvas de fondo negro.

--- alpha_game/js/snake_game.js
+++ alpha_game/js/snake_game.js
@@ -1,17 +1,17 @@
-import {} from "https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.5.0/p5.min.js";
+// import * as mySnake from "https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.5.0/p5.min.js";
 
-window.onload = function() {
-    const COLUMNAS = 40;
-    const FILAS = 30;
-    const LADO = 20;
-    const ANCHO_CANVAS = COLUMNAS * LADO;
-    const ALTO_CANVAS = FILAS * LADO;
+// window.onload = function() {
+//     const COLUMNAS = 40;
+//     const FILAS = 30;
+//     const LADO = 20;
+//     const ANCHO_CANVAS = COLUMNAS * LADO;
+//     const ALTO_CANVAS = FILAS * LADO;
 
-    function setup() {
-        createCanvas(ANCHO_CANVAS, ALTO_CANVAS);
-    }
+//     function setup() {
+//         mySnake.createCanvas(ANCHO_CANVAS, ALTO_CANVAS);
+//     }
 
-    function draw() {
-        background("black");
-    }
-};
+//     function draw() {
+//         mySnake.background("black");
+//     }
+// };
